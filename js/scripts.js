94063972ba221583ed1560f0e5fcf0b368b77a7c
Jo <joannadawnmiller@gmail.com>
2021-01-14 16:29:00 -0500

    add validation to our form, line 28 of js make not show
    Co-authored-by: Tyler Sinks <tyler.sinks@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  let forms = document.getElementsByClassName('needs-validation');
+  var validation = Array.prototype.filter.call(forms, function(form) {
+    form.addEventListener('submit', function(event) {
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+  $("#formOne").submit(function(event, forms) {
+    event.preventDefault();
+    const person1Input = $("input#person1").val();
+    const person2Input = $("input#person2").val();
+    const animalInput= $("input#animal").val();
+    const exclamationInput = $("input#exclamation").val();
+    const verbInput = $("input#verb").val();
+    const nounInput = $("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+    
+    if (forms.classList.includes('was-validated')) {
+      $("#story").show();
+    }
+  });
+});
